fb9a0775f36d52080552ced1540a1291ae3fbc77
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-08-15 19:48:16 +0200

    Render a Google map on the page

---
+++ src/Components/Map.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+  } from "react-google-maps";
+  
+class Map extends Component {
+    render() {
+        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+            <GoogleMap
+                defaultZoom={13}
+                defaultCenter={{ lat: 50.06465, lng: 19.94498 }}
+            >
+            <Marker
+                position={{ lat: 50.06465, lng: 19.94498 }}
+            />
+            </GoogleMap>
+        ));
+      
+        return(
+            <div>
+                <MapWithAMarker
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDziy5R3lKj_zp1jOfiuH-TAncmOqG1MGo&v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+            </div>
+        )
+    }
+}
+
+export default Map
